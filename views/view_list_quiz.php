8331aeadb745558b299d246364d90b3ed19891e0
aurelien <auredoh1@gmail.com>
2024-11-15 15:51:25 +0100

    [list_quiz.css, view_list_quiz.css] modifications et ajout de css des pages entre crochets

--- views/view_list_quiz.php
+++ views/view_list_quiz.php
@@ -12,36 +12,36 @@
     <h2>Liste des Quiz</h2>
     <div class="gallery">
         <div class="boxquiz">
-            <a href="#"><img src="../imgs/logophp.png" alt="PHP"></a>
-            <h2>PHP</h2>
+            <a href="#"><img src="/imgs/logophp.png" alt="PHP"></a>
+            <h3 class="titlequiz">PHP</h3>
         </div>
         <div class="boxquiz">
-            <a href="#"><img src="image2.jpg" alt="Les métiers frontend"></a>
-            <h2 class="quizztitle">Les métiers frontend</h2>
+            <a href="#"><img src="image2.jpg" alt="Les metiers frontend"></a>
+            <h3 class="titlequiz">Les metiers frontend</h3>
         </div>
         <div class="boxquiz">
             <a href="#"><img src="image3.jpg" alt="HTML"></a>
-            <h2>HTML</h2>
+            <h3 class="titlequiz">HTML</h>
         </div>
         <div class="boxquiz">
             <a href="#"><img src="image3.jpg" alt="SQL"></a>
-            <h2>SQL</h2>
+            <h3 class="titlequiz">SQL</h3>
         </div>
         <div class="boxquiz">
             <a href="#"><img src="image3.jpg" alt="Javascript"></a>
-            <h2>Javascript</h2>
+            <h3 class="titlequiz">Javascript</h3>
         </div>
         <div class="boxquiz">
             <a href="#"><img src="image3.jpg" alt="React"></a>
-            <h2>React</h2>
+            <h3 class="titlequiz">React</h3>
         </div>
         <div class="boxquiz">
             <a href="#"><img src="image3.jpg" alt="Docker"></a>
-            <h2>Docker</h2>
+            <h3 class="titlequiz">Docker</h3>
         </div>
         <div class="boxquiz">
             <a href="#"><img src="image3.jpg" alt="Bootstrap"></a>
-            <h2>Bootstrap</h>
+            <h3 class="titlequiz">Bootstrap</h3>
         </div>
 
     </main>
