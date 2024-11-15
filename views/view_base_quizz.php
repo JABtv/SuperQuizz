8a16ef7372bc44c1718983bf37d71ca5ada587fa
SarahRichou <sarah.richou@hotmail.fr>
2024-11-15 16:29:04 +0100

    [view_base_quizz.php] Ajout lien css et ajout classe inputBox aux div avec label et input (ou textarea)

--- views/view_base_quizz.php
+++ views/view_base_quizz.php
@@ -5,21 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SuperQuizz - Nouveau Quizz</title>
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/base_quizz.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 </head>
 <main>
     <h2>Nouveau Quizz</h2>
     <form method="POST">
-        <label for="title">Titre</label>
-        <input type="text" name="title" id="title"/>
+        <div class="inputBox">
+            <label for="title">Titre</label>
+            <input type="text" name="title" id="title"/>
+        </div>
+        <div class="inputBox">
         <label for="description">Description</label>
-        <textarea name="description" id="description" rows="5" cols="30">  
-        </textarea>
-        <label for="image">Image</label>
-        <div>
-            <i class="fa-solid fa-image" style=""></i>
-            <input type="text" name="image" id="image"/>
+            <textarea name="description" id="description" rows="5" cols="30">  
+            </textarea>
+        </div>
+        <div class="inputBox">
+            <label for="image">Image</label>
+            <div>
+                <i class="fa-solid fa-image" style=""></i>
+                <input type="text" name="image" id="image"/>
+            </div>
         </div>
         <div id="categoryChoice">
             <label for="frontend"><input type="radio" name="category" id="frontend">Frontend</label>
