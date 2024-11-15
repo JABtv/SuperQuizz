e475997123195db30b59e7440af986d8a54e830e
SarahRichou <sarah.richou@hotmail.fr>
2024-11-15 14:18:30 +0100

    [access_create.php] Ajout et inclusion fonction sanitize et les views view_header, view_access_create.php et view_footer /// [view_access_create.php] Ajout et Incorporation HTML de access_create, correspond mockup "Acces creation de quizz"

---
+++ views/view_access_create.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuperQuizz - Accès formateur</title>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+    <header></header>
+    <main>
+        <div>
+            <h2>Accéder aux quizz</h2>
+            <button>Jouer</button>
+        </div>
+        <div>
+            <h2>Créer un quizz</h2>
+            <button>Créer</button>
+        </div>
+    </main>
+    
+</body>
+</html>
