0a38421b3cc134838cb23fa7c275d625f2109b37
SarahRichou <sarah.richou@hotmail.fr>
2024-11-15 15:43:00 +0100

    [view_access_create.php] Ajout lien style vers accesse_create.css /// suppression balises header, /body et /html /// ajout classe buttonBox aux div contenant les boutons

--- views/view_access_create.php
+++ views/view_access_create.php
@@ -5,19 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SuperQuizz - Accès formateur</title>
     <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/access_create.css">
 </head>
 <body>
-    <header></header>
     <main>
-        <div>
+        <div class="buttonBox">
             <h2>Acceder aux quizz</h2>
             <button>Jouer</button>
         </div>
-        <div>
+        <div class="buttonBox">
             <h2>Creer un quizz</h2>
             <button>Creer</button>
         </div>
     </main>
-    
-</body>
-</html>
