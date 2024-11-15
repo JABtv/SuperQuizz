ad33e4b754d75eef8dd7c780e61cded84f856e46
SarahRichou <sarah.richou@hotmail.fr>
2024-11-15 14:47:45 +0100

    [view_confirmation.php] Contenu HTML correspondant au mockup 'Confirmation creation de quizz'

---
+++ views/view_confirmation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SuperQuizz - Nouveau Quizz</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+<main>
+    <h2>Quizz PHP créé avec succès!</h2>
+    <button>Retour a la page d'accueil</button>
+</main>
