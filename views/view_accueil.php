3c69564ce63a33067345b2c60700cd7bdda3a2ee
JABtv <tom.andres31000@gmail.com>
2024-11-15 11:32:57 +0100

    suppression du dossier controller et les controllers passe dans la racine du projet

--- views/view_accueil.php
+++ views/view_accueil.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SuperQuizz - Home</title>
-    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="./style/accueil.css">
 </head>
 <main>
@@ -21,10 +21,10 @@
         </div>
         <div class="btn">
             <button>
-                <a href="#">Connexion</a>
+                <a href="superQuizz/controllers/controller_connexion.php">Connexion</a>
             </button>
             <button>
-                <a href="#">Inscription</a>
+                <a href="../controller_inscription.php">Inscription</a>
             </button>
         </div>
     </div>
