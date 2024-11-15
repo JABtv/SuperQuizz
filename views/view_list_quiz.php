910306070f91d41d7bbe4bb74fb153d3859f88be
aurelien <auredoh1@gmail.com>
2024-11-15 15:27:45 +0100

    [controller_view_access_play.php, view_access_play.php, access_play.css, controller_list_quiz.php, view_list_quiz.php, list_quiz.css] Création des pages entre crochet version pc, début du code html et css de la page list_quiz

---
+++ views/view_list_quiz.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/list_quiz.css">
+    <title>Liste des quiz</title>
+</head>
+<body>
+    <main>
+    <h2>Liste des Quiz</h2>
+    <div class="gallery">
+        <div class="boxquiz">
+            <a href="#"><img src="../imgs/logophp.png" alt="PHP"></a>
+            <h2>PHP</h2>
+        </div>
+        <div class="boxquiz">
+            <a href="#"><img src="image2.jpg" alt="Les métiers frontend"></a>
+            <h2 class="quizztitle">Les métiers frontend</h2>
+        </div>
+        <div class="boxquiz">
+            <a href="#"><img src="image3.jpg" alt="HTML"></a>
+            <h2>HTML</h2>
+        </div>
+        <div class="boxquiz">
+            <a href="#"><img src="image3.jpg" alt="SQL"></a>
+            <h2>SQL</h2>
+        </div>
+        <div class="boxquiz">
+            <a href="#"><img src="image3.jpg" alt="Javascript"></a>
+            <h2>Javascript</h2>
+        </div>
+        <div class="boxquiz">
+            <a href="#"><img src="image3.jpg" alt="React"></a>
+            <h2>React</h2>
+        </div>
+        <div class="boxquiz">
+            <a href="#"><img src="image3.jpg" alt="Docker"></a>
+            <h2>Docker</h2>
+        </div>
+        <div class="boxquiz">
+            <a href="#"><img src="image3.jpg" alt="Bootstrap"></a>
+            <h2>Bootstrap</h>
+        </div>
+
+    </main>
+    
+</body>
+</html>
