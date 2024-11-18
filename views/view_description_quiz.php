ed72d9c1610bb76d861ba7139488214d447fa9af
aurel <auredoh1@gmail.com>
2024-11-18 14:45:48 +0100

    [description_quiz.css, view_description.php, controller_description.php] Création, modification et fin des pages html et css de la page description du quiz (quand le stagiaire a choisi un quiz)

---
+++ views/view_description_quiz.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/description_quiz.css">
+    <title>Description du quiz</title>
+</head>
+<body>
+    <main>
+        <div class="upside">
+            <h2>Description</h2>
+            <p class="category">Frontend</p>
+        </div>
+        <div class="boxdescription">
+            <img src="./src/imgs/js_logo.png" alt="logo quizz">
+            <p>JavaScript est un langage de programmation de scripts principalement employé dans les pages web interactives et à ce titre est une partie essentielle des applications web. Avec les langages HTML et CSS, JavaScript est au cœur des langages utilisés par les développeurs web.</p>
+        </div>
+        <div class="btndescription">
+            <button>
+                <a href="controller_connexion.php">Commencer</a>
+            </button>
+    </main>
+
+</body>
+</html>
